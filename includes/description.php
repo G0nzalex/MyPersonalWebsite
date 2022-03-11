956ff4bfcf6f6964949679cfb496f12ddec2a0b5
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-03-11 16:50:53 +0100

    Splitting html code, displaying only content

--- includes/description.php
+++ includes/description.php
@@ -1,163 +1,107 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Qui suis-je ? / Alexandre Nicaise</title>
-    <link rel="stylesheet" href="css/ElmtsCommuns.css">
-    <link rel="stylesheet" href="css/description.css">
-    <link rel="stylesheet" href="css/menu.css">
-    <script src="js/jquery-3.6.0.min.js"></script>
-</head>
-
-<body>
-    <header>
-        <!-- En tête dans lequel figurent mon nom, mon logo et mon menu -->
-        <img src="img/Logo-webdesign.png" alt="Mon logo" id="logo">
-        <div class="burger-button">
-            <span class="burger-top"></span>
-            <span class="burger-middle"></span>
-            <span class="burger-bottom"></span>
-        </div>
-        <div class="burger-menu">
-            <a href="index.html">Accueil</a>
-            <a href="Description.html" class="important">Qui suis-je ?</a>
-            <a href="Contact.html">Contact</a>
-            <a href="Alexandre-Nicaise.pdf" download="CV.pdf">Mon CV</a>
-        </div>
-        <nav class="navTablet">
-            <a href="index.html">Accueil</a>
-            <a href="Description.html" class="important">Qui suis-je ?</a>
-            <a href="Contact.html">Contact</a>
-        </nav>
-        <div class="identite">
-            <h1 id="monNom">Alexandre Nicaise</h1>
-            <h2 id="monSlogan">Tel que vous le voyez</h2>
-        </div>
-        <nav class="navLaptop">
-            <a href="index.html">Accueil</a>
-            <a href="Description.html" class="important">Qui suis-je ?</a>
-            <a href="Contact.html">Contact</a>
-        </nav>
-    </header>
-    <main>
-        <!-- Le corps du site dans lequel figurent les informations principales -->
-        <div class="main">
-            <h1 class="cache">Qui suis-je ?</h1>
-            <section class="presentation cache">
-                <h2 class="hidden">Ma présentation</h2>
-                <blockquote>
-                    "A mes yeux, l’identité est une façon de se gagner un passeport pour le monde."<br>
-                    Marcello Fois
-                </blockquote>
-                <p><strong>Titulaire d'un bac Sciences et technologies de l'industrie et du développement
-                        durable</strong>
-                    et actuellement en <strong>formation de développement web et web mobile à la CCI portes de
-                        Normandie</strong>, je fais mes premiers pas dans le monde du travail. </p>
-            </section>
-            <section class="pro">
-                <h2 class="cache">Mon parcours professionnelle</h2>
-                <!-- Les tableaux cliquables -->
-                <div class="tableaux cache">
-                    <table class="t1">
-                        <thead>
-                            <tr>
-                                <th colspan="3">Diplômes et certifications</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>Nom</th>
-                                <th>Lieu/Organisme</th>
-                                <th>Date</th>
-                            </tr>
-                            <tr class="event1 event">
-                                <td>Bac STI2D</td>
-                                <td>Lycée Georges Dumezil</td>
-                                <td>Juillet 2020</td>
-                            </tr>
-                            <tr class="event2 event">
-                                <td>Bootcamp Get Into Tech</td>
-                                <td>Inco Academy</td>
-                                <td>Avril 2021</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <table class="t2">
-                        <thead>
-                            <tr>
-                                <th colspan="3">Expériences professionnelles</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>Nature</th>
-                                <th>Lieu/Organisme</th>
-                                <th>Date</th>
-                            </tr>
-                            <tr class="event3 event">
-                                <td>Bénévolat</td>
-                                <td>Restos du Coeur</td>
-                                <td>De Janvier à Mars 2021</td>
-                            </tr>
-                        </tbody>
-                    </table>
+<main>
+    <!-- Le corps du site dans lequel figurent les informations principales -->
+    <div class="main">
+        <h1 class="cache">Qui suis-je ?</h1>
+        <section class="presentation cache">
+            <h2 class="hidden">Ma présentation</h2>
+            <blockquote>
+                "A mes yeux, l’identité est une façon de se gagner un passeport pour le monde."<br>
+                Marcello Fois
+            </blockquote>
+            <p><strong>Titulaire d'un bac Sciences et technologies de l'industrie et du développement
+                    durable</strong>
+                et actuellement en <strong>formation de développement web et web mobile à la CCI portes de
+                    Normandie</strong>, je fais mes premiers pas dans le monde du travail. </p>
+        </section>
+        <section class="pro">
+            <h2 class="cache">Mon parcours professionnelle</h2>
+            <!-- Les tableaux cliquables -->
+            <div class="tableaux cache">
+                <table class="t1">
+                    <thead>
+                        <tr>
+                            <th colspan="3">Diplômes et certifications</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Nom</th>
+                            <th>Lieu/Organisme</th>
+                            <th>Date</th>
+                        </tr>
+                        <tr class="event1 event">
+                            <td>Bac STI2D</td>
+                            <td>Lycée Georges Dumezil</td>
+                            <td>Juillet 2020</td>
+                        </tr>
+                        <tr class="event2 event">
+                            <td>Bootcamp Get Into Tech</td>
+                            <td>Inco Academy</td>
+                            <td>Avril 2021</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="t2">
+                    <thead>
+                        <tr>
+                            <th colspan="3">Expériences professionnelles</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Nature</th>
+                            <th>Lieu/Organisme</th>
+                            <th>Date</th>
+                        </tr>
+                        <tr class="event3 event">
+                            <td>Bénévolat</td>
+                            <td>Restos du Coeur</td>
+                            <td>De Janvier à Mars 2021</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- Les détails qui apparaissent après un clic -->
+            <div class="case1 cases">
+                <h3>Bac Sciences et technologies de l'industrie et du développement durable</h3>
+                <div class="bouton">
+                    <span class="trait1"></span>
+                    <span class="trait2"></span>
                 </div>
-                <!-- Les détails qui apparaissent après un clic -->
-                <div class="case1 cases">
-                    <h3>Bac Sciences et technologies de l'industrie et du développement durable</h3>
-                    <div class="bouton">
-                        <span class="trait1"></span>
-                        <span class="trait2"></span>
-                    </div>
-                    <p>Après avoir fait une Seconde Générale, je suis passé en filière technologique pour obtenir le
-                        bac. <br>
-                        La filière générale ne m'intéressait pas plus que cela et je souhaitais faire des études plus
-                        proches de mes centres d'intérêt.</p>
+                <p>Après avoir fait une Seconde Générale, je suis passé en filière technologique pour obtenir le
+                    bac. <br>
+                    La filière générale ne m'intéressait pas plus que cela et je souhaitais faire des études plus
+                    proches de mes centres d'intérêt.</p>
+            </div>
+            <div class="case2 cases">
+                <h3>Bootcamp Get into Tech</h3>
+                <div class="bouton">
+                    <span class="trait1"></span>
+                    <span class="trait2"></span>
                 </div>
-                <div class="case2 cases">
-                    <h3>Bootcamp Get into Tech</h3>
-                    <div class="bouton">
-                        <span class="trait1"></span>
-                        <span class="trait2"></span>
-                    </div>
-                    <p>Je souhaitais faire des études en rapport avec l'informatique, les ordinateurs ou les jeux
-                        vidéos. <br>
-                        Mais c'était des domaines encore assez flous à mes yeux. Je me suis intéressé à la création de
-                        sites web et je voulais savoir si cela était réellement fait pour moi.<br>
-                        J'ai donc intégré <span class="phone">en Avril 2021 à l'Inco Academy </span>le bootcamp Get into
-                        Tech
-                        dans lequel j'ai découvert le HTML/CSS, Wordpress ainsi que quelques notions en marketing. <br>
-                        Á la fin du bootcamp, j'étais convaincu que je voulais apprendre à coder et programmer des
-                        sites web.</p>
+                <p>Je souhaitais faire des études en rapport avec l'informatique, les ordinateurs ou les jeux
+                    vidéos. <br>
+                    Mais c'était des domaines encore assez flous à mes yeux. Je me suis intéressé à la création de
+                    sites web et je voulais savoir si cela était réellement fait pour moi.<br>
+                    J'ai donc intégré <span class="phone">en Avril 2021 à l'Inco Academy </span>le bootcamp Get into
+                    Tech
+                    dans lequel j'ai découvert le HTML/CSS, Wordpress ainsi que quelques notions en marketing. <br>
+                    Á la fin du bootcamp, j'étais convaincu que je voulais apprendre à coder et programmer des
+                    sites web.</p>
+            </div>
+            <div class="case3 cases">
+                <h3>Bénévole dans Les Restos du Coeur</h3>
+                <div class="bouton">
+                    <span class="trait1"></span>
+                    <span class="trait2"></span>
                 </div>
-                <div class="case3 cases">
-                    <h3>Bénévole dans Les Restos du Coeur</h3>
-                    <div class="bouton">
-                        <span class="trait1"></span>
-                        <span class="trait2"></span>
-                    </div>
-                    <p>J'ai réalisé, dans le cadre de la Garantie Jeune (Mission Locale), un stage de deux semaines dans
-                        Les Restos Du Coeur.<br>
-                        J'ai notamment aidé à trier et à ranger les ressources, les donner aux adhérents et les
-                        collecter dans des magasins.<br>
-                        Une fois le stage terminé, j'ai continué à leur fournir mon aide jusqu'à la collecte nationale
-                        annuelle qui a eu lieu en Mars 2021. </p>
-                </div>
-            </section>
-        </div>
-    </main>
-    <footer>
-        <!-- Bas de page -->
-        <div class="footer">
-            <p>Vous souhaitez consulter mon CV ? Téléchargez le ici : </p>
-            <a href="Alexandre-Nicaise.pdf" download="CV.pdf">Mon CV</a>
-            <!-- Attention, download n'est pas reconnu par tous les navigateurs -->
-        </div>
-    </footer>
-    <script src="js/menu.js"></script>
-</body>
-
-</html>
+                <p>J'ai réalisé, dans le cadre de la Garantie Jeune (Mission Locale), un stage de deux semaines dans
+                    Les Restos Du Coeur.<br>
+                    J'ai notamment aidé à trier et à ranger les ressources, les donner aux adhérents et les
+                    collecter dans des magasins.<br>
+                    Une fois le stage terminé, j'ai continué à leur fournir mon aide jusqu'à la collecte nationale
+                    annuelle qui a eu lieu en Mars 2021. </p>
+            </div>
+        </section>
+    </div>
+</main>
