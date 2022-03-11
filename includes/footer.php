4f3a4f8f7e6e28b57bf481a16b4bd8693ed96585
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-03-11 16:49:22 +0100

    Adding the footer of the website

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <!-- Bas de page -->
+        <div class="footer">
+            <p>Vous souhaitez consulter mon CV ? Téléchargez le ici : </p>
+            <a href="Alexandre-Nicaise.pdf" download="CV.pdf">Mon CV</a>
+            <!-- Attention, download n'est pas reconnu par tous les navigateurs -->
+        </div>
+    </footer>
+    <script src="js/menu.js"></script>
+</body>
