edf982e794110c959f2d42c4de8f56bdbe2a8429
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-03-12 21:54:00 +0100

    Url links have been fixed

--- includes/nav.php
+++ includes/nav.php
@@ -1,29 +1,22 @@
-<header>
-    <!-- En tête dans lequel figurent mon nom, mon logo et mon menu -->
-    <img src="img/Logo-webdesign.png" alt="Mon logo" id="logo">
-    <div class="burger-button">
-        <span class="burger-top"></span>
-        <span class="burger-middle"></span>
-        <span class="burger-bottom"></span>
-    </div>
-    <div class="burger-menu">
-        <a href="index.html">Accueil</a>
-        <a href="Description.html" class="important">Qui suis-je ?</a>
-        <a href="Contact.html">Contact</a>
-        <a href="Alexandre-Nicaise.pdf" download="CV.pdf">Mon CV</a>
-    </div>
-    <nav class="navTablet">
-        <a href="index.html">Accueil</a>
-        <a href="Description.html" class="important">Qui suis-je ?</a>
-        <a href="Contact.html">Contact</a>
-    </nav>
-    <div class="identite">
-        <h1 id="monNom">Alexandre Nicaise</h1>
-        <h2 id="monSlogan">Tel que vous le voyez</h2>
-    </div>
-    <nav class="navLaptop">
-        <a href="index.html">Accueil</a>
-        <a href="Description.html" class="important">Qui suis-je ?</a>
-        <a href="Contact.html">Contact</a>
-    </nav>
-</header>
+    <!-- Menu de navigation -->
+<div class="burger-button">
+    <span class="burger-top"></span>
+    <span class="burger-middle"></span>
+    <span class="burger-bottom"></span>
+</div>
+<div class="burger-menu">
+    <a href="index.php?page=accueil">Accueil</a>
+    <a href="index.php?page=description" class="important">Qui suis-je ?</a>
+    <a href="index.php?page=formulaire">Contact</a>
+    <a href="Alexandre-Nicaise.pdf" download="CV.pdf">Mon CV</a>
+</div>
+<nav class="navTablet">
+    <a href="index.php?page=accueil">Accueil</a>
+    <a href="index.php?page=description" class="important">Qui suis-je ?</a>
+    <a href="index.php?page=formulaire">Contact</a>
+</nav>
+<nav class="navLaptop">
+    <a href="index.php?page=accueil">Accueil</a>
+    <a href="index.php?page=description" class="important">Qui suis-je ?</a>
+    <a href="index.php?page=formulaire">Contact</a>
+</nav>
