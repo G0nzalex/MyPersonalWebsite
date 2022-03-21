a21728b5994b9fbe899de8c794f875b1c0255bdf
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-03-21 21:26:59 +0100

    Back to original code

--- includes/formulaire.inc.php
+++ includes/formulaire.inc.php
@@ -1,61 +1,55 @@
 <?php
-// if (isset($_POST['validation'])) {
-//     // Vérifications côté serveur
-//     $nom = htmlentities(trim($_POST['nom'])) ?? '';
-//     $prenom = htmlentities(trim($_POST['prenom'])) ?? '';
-//     $email = htmlentities(trim($_POST['email'])) ?? '';
-//     $sujet = htmlentities(trim($_POST['sujet'])) ?? '';
-//     $message = htmlentities(trim($_POST['message'])) ?? '';
-//     // Tableaux des erreurs possibles
-//     $erreur = array();
+if (isset($_POST['validation'])) {
+    // Vérifications côté serveur
+    $nom = htmlentities(trim($_POST['nom'])) ?? '';
+    $prenom = htmlentities(trim($_POST['prenom'])) ?? '';
+    $email = htmlentities(trim($_POST['email'])) ?? '';
+    $sujet = htmlentities(trim($_POST['sujet'])) ?? '';
+    $message = htmlentities(trim($_POST['message'])) ?? '';
+    // Tableaux des erreurs possibles
+    $erreur = array();
 
-//     if (strlen($nom) === 0)
-//         array_push($erreur, "Veuillez saisir votre nom");
+    if (strlen($nom) === 0)
+        array_push($erreur, "Veuillez saisir votre nom");
 
-//     if (strlen($prenom) === 0)
-//         array_push($erreur, "Veuillez saisir votre prénom");
+    if (strlen($prenom) === 0)
+        array_push($erreur, "Veuillez saisir votre prénom");
 
-//     if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-//         array_push($erreur, "Veuillez saisir un e-mail valide");
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+        array_push($erreur, "Veuillez saisir un e-mail valide");
 
-//     if (strlen($sujet) === 0)
-//         array_push($erreur, "Veuillez saisir le sujet de votre message");
+    if (strlen($sujet) === 0)
+        array_push($erreur, "Veuillez saisir le sujet de votre message");
 
-//     if (strlen($message) === 0)
-//         array_push($erreur, "Veuillez saisir un message");
+    if (strlen($message) === 0)
+        array_push($erreur, "Veuillez saisir un message");
 
-//     // Envoi du mail
-//     if (count($erreur) === 0) {
-//         $from = $email;
-//         $to = "alexandrecalimero@gmail.com";
-//         $subject = $sujet;
-//         $header = "De:" . $from;
-//         mail($to, $subject, $message, $header);
-//         echo "Message envoyé";
+    // Envoi du mail
+    if (count($erreur) === 0) {
+        $from = $email;
+        $to = "alexandrecalimero@gmail.com";
+        $subject = $sujet;
+        $header = "De:" . $from;
+        mail($to, $subject, $message, $header);
+        echo "Message envoyé";
         
-//         // Renvoi d'un message en cas d'erreur
-//     } else {
-//         $messageErreur = "<ul>";
-//         $i = 0;
-//         do {
-//             $messageErreur .= "<li>";
-//             $messageErreur .= $erreur[$i];
-//             $messageErreur .= "</li>";
-//             $i++;
-//         } while ($i < count($erreur));
+        // Renvoi d'un message en cas d'erreur
+    } else {
+        $messageErreur = "<ul>";
+        $i = 0;
+        do {
+            $messageErreur .= "<li>";
+            $messageErreur .= $erreur[$i];
+            $messageErreur .= "</li>";
+            $i++;
+        } while ($i < count($erreur));
 
-//         $messageErreur .= "</ul>";
+        $messageErreur .= "</ul>";
 
-//         echo $messageErreur;
-//     }
-// } else {
-//     echo "Merci de renseigner le formulaire";
-//     $nom = $prenom = $email = $message = '';
-// }
-require './classes/Formulaire.php';
-$moi = new Formulaire();
-var_dump($moi);
-$moi->setNom("alex");
-$nom = $moi->getNom();
-var_dump($nom);
+        echo $messageErreur;
+    }
+} else {
+    echo "Merci de renseigner le formulaire";
+    $nom = $prenom = $email = $message = '';
+}
 include 'contact.php';
