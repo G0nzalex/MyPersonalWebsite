58b8f1644c2603a558484edfb70aea4bc3cd52a8
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-04-05 22:51:05 +0200

    function() added to push string into array

--- classes/Erreurs.php
+++ classes/Erreurs.php
@@ -6,4 +6,9 @@ trait Erreurs
     {
         return strlen($entree) === 0 ? $msgErreur : "";
     }
+    public function compteur(array &$erreurs, string $messageErreur) : void
+    {
+        if (isset($erreurs) && strlen($messageErreur) !== 0)
+            array_push($erreurs, "");
+    }
 }
